016db8f49b6a6c84161102fd00d02c7abbbd27a3
boredStats <ian.abenes@gmail.com>
2019-02-18 15:25:10 -0600

    changed downsampling procedure
    MEG timeseries truncated to subject with shortest timeseries

--- python scripts/downsample_MEG.py
+++ python scripts/downsample_MEG.py
@@ -9,6 +9,7 @@ sys.path.append("..")
 
 import os
 import time
+import h5py
 import datetime
 import proj_utils as pu
 from scipy import signal
@@ -30,6 +31,7 @@ sess_list = sorted(list(set([f.split('_')[-1].replace('.mat', '') for f in filel
 #Define constants
 meg_original_timepoints = 745619
 meg_downsampled_timepoints = 146592
+min_meg_length = 118088
 meg_original_sampling_rate = 2034.51 
 #meg_downsampled_sampling_rate = meg_downsampled_timepoints / meg_original_timepoints * meg_original_sampling_rate
 meg_downsampled_sampling_rate = 500 #from Hye
@@ -38,7 +40,7 @@ rsfmri_sampling_rate = 1/.72
 rsfmri_timepoints = 1200
 rsfmri_time = rsfmri_timepoints / rsfmri_sampling_rate
 
-meg_con = (meg_downsampled_timepoints * rsfmri_sampling_rate ) / meg_downsampled_sampling_rate
+meg_con = (min_meg_length * rsfmri_sampling_rate ) / meg_downsampled_sampling_rate
 
 #Load example timeesries
 subj = subj_list[0]
@@ -49,21 +51,21 @@ meg_rois = pu.read_database(meg_dset, roi_labels)
 meg_resampled_timeseries = signal.resample(meg_rois.values, int(meg_con))
 
 #Resample MEG data, append to hdf5 file
-subject_timepoints_list = []
+downsamp_path = pdir + '/data/downsampled_MEG_truncated.hdf5'
+
 for sess in sess_list:
     for subj in subj_list:
         dset_to_load = database['/' + subj + '/MEG/' + sess + '/timeseries']
         meg_data = pu.read_database(dset_to_load, roi_labels)
-        subject_timepoints_list.append(meg_data.values.shape)
         
-        meg_data_resampled = signal.resample(meg_data.values, int(meg_con))
+        meg_data_resampled = signal.resample(meg_data.values[:min_meg_length, :], int(meg_con))
+        downsamp_file = h5py.File(downsamp_path)
+        grp = downsamp_file.require_group('/' + subj + '/MEG/' + sess)
         
-        grp = database.require_group('/' + subj + '/MEG/' + sess)
-        
-        dset = grp.create_dataset('resampled',
+        dset = grp.create_dataset('resampled_truncated',
                                   data=meg_data_resampled,
                                   chunks=True, compression='gzip')
-        
+        downsamp_file.close()
         st = datetime.datetime.fromtimestamp(time.time())
         print('Subject ' + subj + ' complete at ' + str(st))
     st = datetime.datetime.fromtimestamp(time.time())
