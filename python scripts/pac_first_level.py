ea072e1e57c5da86c4b05046a8943a6d83d62544
boredStats <ian.abenes@gmail.com>
2019-04-22 16:17:45 -0500

    Revert "quick change to only extract delta/theta phase for now"
    This reverts commit f7999ac2005a4ea90bd090091884658680a535c6.

--- python scripts/pac_first_level.py
+++ python scripts/pac_first_level.py
@@ -40,21 +40,14 @@ meg_subj, meg_sess = pdObj.get_meg_metadata()
 fs = 500
 comp = 'lzf' #h5py compression param
 
-#Full combination of phase-amplitude coupling
-phase_bands = ['Delta', 'Theta']#list(band_dict)
-amp_bands = list(band_dict)
-
 # data_path = pdir + '/data/MEG_BOLD_phase_amp_data.hdf5'
-# data_path = pdir + '/data/MEG_phase_amp_data.hdf5'
-data_path = pdir + '/data/MEG_phase_amp_data_delta_theta.hdf5'
+data_path = pdir + '/data/MEG_phase_amp_data.hdf5'
 check = input('Extract phase/amplitude data? y/n ')
 if check=='y':
     print('%s: Extracting phase/amp data' % pu.ctime())
     for sess in meg_sess:
         for subj in meg_subj:
             for b in band_dict:
-                if b not in phase_bands:
-                    continue
                 band = band_dict[b]
 
                 out_file = h5py.File(data_path)
@@ -78,53 +71,53 @@ if check=='y':
                     compression=comp)
                 out_file.close()
 
-# print('%s: Running phase-amplitdue coupling' % pu.ctime())
+print('%s: Running phase-amplitdue coupling' % pu.ctime())
 # coupling_path = pdir + '/data/MEG_BOLD_phase_amp_coupling.hdf5' #BOLD only
-# coupling_path = pdir + '/data/MEG_phase_amp_coupling_delta_theta.hdf5' #all combinations
-#
-# # #BOLD bandpass with higher frequency only
-# # slow_bands = ['BOLD bandpass']
-# # reg_bands = ['Delta', 'Theta', 'Alpha', 'Beta', 'Gamma']
-#
-# #Full combination of phase-amplitude coupling
-# phase_bands = ['Delta', 'Theta']#list(band_dict)
-# amp_bands = list(band_dict)
-#
-# for sess in meg_sess:
-#     for subj in meg_subj:
-#         data_file = h5py.File(data_path, 'r')
-#         subj_data = data_file.get(subj + '/' + sess)
-#         for r, roi in enumerate(rois):
-#             cfc_file = h5py.File(coupling_path)
-#             group_path = sess + '/' + subj + '/' + roi
-#             if group_path in cfc_file:
-#                 continue #check if work has already been done
-#
-#             print('%s: CFC for %s %s %s' % (pu.ctime(), sess, str(subj), roi))
-#             r_mat = np.ndarray(shape=(len(phase_bands), len(amp_bands)))
-#             p_mat = np.ndarray(shape=(len(phase_bands), len(amp_bands)))
-#             for phase_index, phase_band in enumerate(phase_bands):
-#                 p_grp = subj_data.get(phase_band)
-#                 phase_spect = p_grp.get('phase_data')[:, r]
-#                 for amp_index, amp_band in enumerate(amp_bands):
-#                     a_grp = subj_data.get(amp_badn)
-#                     amp_spect = a_grp.get('amplitude_data')[:, r]
-#
-#                     r_val, p_val = pac.circCorr(phase_spect, amp_spect)
-#                     r_mat[phase_index, amp_index] = r_val
-#                     p_mat[phase_index, amp_index] = p_val
-#
-#             out_group = cfc_file.require_group(group_path)
-#             out_group.create_dataset(
-#                 'r_vals',
-#                 data=r_mat,
-#                 compression=comp)
-#             out_group.create_dataset(
-#                 'p_vals',
-#                 data=p_mat,
-#                 compression=comp)
-#             cfc_file.close()
-#
-#         phase_amp_file.close()
+coupling_path = pdir + '/data/MEG_phase_amp_coupling.hdf5' #all combinations
+
+# #BOLD bandpass with higher frequency only
+# slow_bands = ['BOLD bandpass']
+# reg_bands = ['Delta', 'Theta', 'Alpha', 'Beta', 'Gamma']
+
+#Full combination of phase-amplitude coupling
+phase_bands = list(band_dict)
+amp_bands = list(band_dict)
+
+for sess in meg_sess:
+    for subj in meg_subj:
+        data_file = h5py.File(data_path, 'r')
+        subj_data = data_file.get(subj + '/' + sess)
+        for r, roi in enumerate(rois):
+            cfc_file = h5py.File(coupling_path)
+            group_path = sess + '/' + subj + '/' + roi
+            if group_path in cfc_file:
+                continue #check if work has already been done
+
+            print('%s: CFC for %s %s %s' % (pu.ctime(), sess, str(subj), roi))
+            r_mat = np.ndarray(shape=(len(phase_bands), len(amp_bands)))
+            p_mat = np.ndarray(shape=(len(phase_bands), len(amp_bands)))
+            for phase_index, phase_band in enumerate(phase_bands):
+                p_grp = subj_data.get(phase_band)
+                phase_spect = p_grp.get('phase_data')[:, r]
+                for amp_index, amp_band in enumerate(amp_bands):
+                    a_grp = subj_data.get(amp_badn)
+                    amp_spect = a_grp.get('amplitude_data')[:, r]
+
+                    r_val, p_val = pac.circCorr(phase_spect, amp_spect)
+                    r_mat[phase_index, amp_index] = r_val
+                    p_mat[phase_index, amp_index] = p_val
+
+            out_group = cfc_file.require_group(group_path)
+            out_group.create_dataset(
+                'r_vals',
+                data=r_mat,
+                compression=comp)
+            out_group.create_dataset(
+                'p_vals',
+                data=p_mat,
+                compression=comp)
+            cfc_file.close()
+
+        phase_amp_file.close()
 
 print('%s: Finished' % pu.ctime())
