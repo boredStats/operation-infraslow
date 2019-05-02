ca1aa2c2bcd6639ec640bb09618b6d828b8dedd6
boredStats <ian.abenes@gmail.com>
2019-05-02 14:02:18 -0500

    Update braintest.py

--- python scripts/braintest.py
+++ python scripts/braintest.py
@@ -58,6 +58,36 @@ def plot_brain_saliences(custom_roi, minval=0, maxval=None, figpath=None, cbar=F
                 colorbar=cbar)
     plt.clf()
 
+def plot_brain_heatmap(plot_matrix, color_list, fname=None):
+    mpl.rcParams['image.interpolation'] = 'nearest'
+
+    cm = LinearSegmentedColormap.from_list(
+        'my_cmap',
+        full_color_list,
+        N=len(full_color_list))
+
+    fig, ax = plt.subplots(figsize=(4, 48))
+    im = ax.matshow(
+        plot_matrix,
+        # interpolation='none',
+        origin='lower',
+        aspect='auto',
+        vmax=7,
+        cmap=cm)
+
+    # ylines = np.arange(1, len(left_rois), 1)
+    # for y in ylines:
+    #     ax.axhline(y, linewidth=.2)
+
+    # ax.set_yticklabels(left_rois[::-1])
+    # fig.colorbar(im)
+
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    ax.tick_params(axis='both', length=0, width=0, labelsize=0)
+
+    fig.savefig(fname, bbox_inches='tight')
+
 if __name__ == '__main__':
     import os
     import numpy as np
@@ -75,13 +105,15 @@ if __name__ == '__main__':
     pdObj = pu.proj_data()
     rois = pdObj.roiLabels
     colors = pdObj.colors
+    bands = ['Delta', 'Theta', 'Alpha', 'Beta', 'Gamma']
+
     alpha_colors = []
     for c in colors:
         color = list(np.divide(c, 255))
         # color.append(1)
         alpha_colors.append(tuple(color))
 
-    ###Plotting saliences on the brain###
+    ###Plotting saliences on the brain
     cmap = 'viridis'
     # plot_brain_saliences(
     #     custom_roi=fig_path + 'brain_LatentVar1.nii',
@@ -91,27 +123,59 @@ if __name__ == '__main__':
     #     cbar=False,
     #     cmap=cmap)
 
-    ###Plotting saliences on a grid###
+    ###Plotting saliences on a heatmap
     cfc_path = fig_path + 'mPLSC_cfc/'
     dir_list = os.listdir(cfc_path)
-    sal_list = [f for f in dir_list if 'conjunction' in f]
+    conjunction_list_unsort = [f for f in dir_list if 'conjunction' in f]
+    raw_saliences_list_unsort = [f for f in dir_list if 'brain_saliences' in f]
+
+    #Sorting files into proper band order
+    conjunction_list, raw_saliences_list = [], []
+    for band in bands:
+        for file in conjunction_list_unsort:
+            if band in file:
+                conjunction_list.append(file)
+        for file in raw_saliences_list_unsort:
+            if band in file:
+                raw_saliences_list.append(file)
 
     #Creating list of latent variable names
     num_latent_vars = 4
     latent_names = ['LatentVar%d' % (n+1) for n in range(num_latent_vars)]
 
     #Getting salience data from excel files
-    sals = {}
-    for sal in sal_list:
-        table_name = sal.replace('.xlsx', '')
-        raw_df = pd.read_excel(os.path.join(cfc_path, sal))
-        df = raw_df[latent_names]
-        sals[table_name] = df
+    conjunction_sals = {}
+    for conj in conjunction_list:
+        table_name = conj.replace('.xlsx', '')
+        raw_df = pd.read_excel(os.path.join(cfc_path, conj))
+        conjunction_sals[table_name] = raw_df[latent_names]
+
+    #Getting average saliences for each band, latent variable
+    average_sals_over_sessions = {}
+    for r, raw in enumerate(raw_saliences_list):
+        average_sals = pd.DataFrame(index=rois, columns=latent_names)
+        xls = pd.ExcelFile(os.path.join(cfc_path, raw))
+
+        session_data = {}
+        for sheet in xls.sheet_names:
+            df = xls.parse(sheet, index_col=0)
+            session_data[sheet] = df[latent_names]
+
+        for name in latent_names:
+            for roi in rois:
+                z_vals = []
+                for sess in list(session_data):
+                    df = session_data[sess]
+                    val = df.loc[roi, name]
+                    z_vals.append(np.abs(val))
+                mu = np.mean(z_vals)
+                average_sals.loc[roi, name] = mu
+        average_sals_over_sessions[bands[r]] = average_sals
 
     #Creating custom colormap
     white = np.array([1, 1, 1])
-    gray = np.multiply([1, 1, 1], .2)
-    colors_for_plot = alpha_colors[:len(sals)]
+    gray = np.multiply([1, 1, 1], .5)
+    colors_for_plot = alpha_colors[:len(conjunction_sals)]
 
     full_color_list = []
     full_color_list.append(white)
@@ -119,59 +183,24 @@ if __name__ == '__main__':
     for plot_color in colors_for_plot:
         full_color_list.append(plot_color)
 
-    cm = LinearSegmentedColormap.from_list(
-        'my_cmap',
-        full_color_list,
-        N=len(full_color_list))
-
-    #Creating plot data as MxNxRGBA array
-    # pixel_data = np.ndarray(shape=(360, len(sals), 4))
-    # for s, sal in enumerate(list(sals)):#name in latent_names:
-    #     color = colors_for_plot[s]
-    #     df = sals[sal]
-    #     test_data = df[latent_names[0]].values
-    #     mu = np.mean(test_data)
-    #     for v, val in enumerate(test_data):
-    #         pixel_data[v, :, :] = v
-    #         pixel_data[:, s, :] = s
-    #         if val < mu:######
-    #             if val < 4:
-    #                 pixel_data[v, s, :] = white
-    #             else:
-    #                 pixel_data[v, s, :] = gray
-    #         else:
-    #             pixel_data[v, s, :] = color
-    # print(pixel_data[0, :, :])
-    # # x = pixel_data[:, 0, 0, 0, 0, 0]
-    # # y = pixel_data[0, :, 0, 0, 0, 0]
-    # # extent = np.min(x), np.max(x), np.min(y), np.max(y)
-    # fig, ax = plt.subplots(figsize=(8, 6))
-    # im = ax.imshow(
-    #     pixel_data,
-    #     interpolation='none',
-    #     origin='lower',
-    #     aspect='auto',
-    #     # extent=extent,
-    #     # vmax=5,
-    #     # cmap=cm
-    #     )
-    #
-    # # fig.colorbar(im)
-    # fig.savefig('test_pixel.png', bbox_inches='tight')
-
     left_rois = [r for r in rois if '_L' in r]
     right_rois = [r for r in rois if '_R' in r]
 
     #Creating plot data as integer list
     for name in latent_names:
         plot_matrix = []
-        for s, sal in enumerate(list(sals)):#name in latent_names:
-            df = sals[sal]
-            test_data = df[name].values
-            mu = np.mean(test_data)
-            print(mu)
+        for s, sal in enumerate(list(conjunction_sals)):
+            #Calulating average z-score for the latent variable
+            band = bands[s]
+            average_sals = average_sals_over_sessions[band]
+            average_sals_in_lv = average_sals[name]
+            mu = np.mean(average_sals_in_lv.values)
+
+            conj_df = conjunction_sals[sal]
+            conj_data = conj_df[name].values
+            #Dummy-coding conjunction z-scores
             plot_data = []
-            for val in test_data:
+            for val in conj_data:
                 if val < mu:
                     if val < 4:
                         plot_data.append(0)
@@ -180,13 +209,14 @@ if __name__ == '__main__':
                 else:
                     plot_data.append(s+2)
             plot_matrix.append(plot_data)
+
+        #Turning nested lists into array
         plot_matrix = np.asarray(plot_matrix).T
-        print(plot_matrix.shape)
-        plot_df = pd.DataFrame(plot_matrix, index=rois, columns=list(sals))
+        plot_df = pd.DataFrame(plot_matrix, index=rois, columns=bands)
 
         lr_plot_data = []
-        for sal in list(sals):
-            series = plot_df[sal]
+        for band in bands:
+            series = plot_df[band]
             left_series = series.loc[left_rois]
             right_series = series.loc[right_rois]
             lr_plot_data.append(left_series.values)
@@ -195,37 +225,15 @@ if __name__ == '__main__':
         lr_plot_matrix = np.flipud(lr_plot_matrix)
         print(lr_plot_matrix.shape)
 
-        mpl.rcParams['image.interpolation'] = 'nearest'
-        fig, ax = plt.subplots(figsize=(4, 48))
-        im = ax.matshow(
-            lr_plot_matrix,
-            # interpolation='none',
-            origin='lower',
-            aspect='auto',
-            vmax=7,
-            cmap=cm)
-        ylines = np.arange(1, len(left_rois), 1)
-        for y in ylines:
-            ax.axhline(y, linewidth=.2)
-        ax.set_xticklabels([])
-        ax.set_yticklabels([])
-        ax.tick_params(axis='both', length=0, width=0, labelsize=0)
-        # ax.set_yticklabels(left_rois[::-1])
-        # fig.colorbar(im)
-        # fname = cfc_path + 'brain_heatmap_%s.svg' % name
-        # fig.savefig(fname, bbox_inches='tight')
-        # plt.show()
+        fname = cfc_path + 'brain_heatmap_%s.svg' % name
+        plot_brain_heatmap(lr_plot_matrix, full_color_list, fname)
 
-    # cleaned_roi_list = [r.replace('_L', '') for r in left_rois]
-    # print(cleaned_roi_list)
-    #
-    # col_1 = cleaned_roi_list[0::3]
-    # col_2 = cleaned_roi_list[1::3]
-    # col_3 = cleaned_roi_list[2::3]
-    #
-    # df = pd.DataFrame(columns=['col1', 'col2', 'col3'])
-    # df['col1'] = col_1
-    # df['col2'] = col_2
-    # df['col3'] = col_3
-    # df.to_excel(fig_path + 'roi_list_3column.xlsx')
-    # print('Finished')
+    #Creating 3-stacked list of ROIs
+    cleaned_roi_list = [r.replace('_L', '') for r in left_rois]
+    df = pd.DataFrame(columns=['col1', 'col2', 'col3'])
+    df['col1'] = cleaned_roi_list[0::3]
+    df['col2'] = cleaned_roi_list[1::3]
+    df['col3'] = cleaned_roi_list[2::3]
+    df.to_excel(fig_path + 'roi_list_3column.xlsx')
+
+    print('Finished')
